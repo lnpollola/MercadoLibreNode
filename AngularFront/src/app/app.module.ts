27ea03d7d3028cedb79ed8a71c815656ed59cbd1
lnpollola <leandro.pollola@hotmail.com>
2019-03-16 19:33:41 -0300

    servicio generico con http, modificar e importar por cada uno

--- AngularFront/src/app/app.module.ts
+++ AngularFront/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//Servicios
+import { GenericoService } from './servicios/HTTP/generico.service';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +15,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [GenericoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
